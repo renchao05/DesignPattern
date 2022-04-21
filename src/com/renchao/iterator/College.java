1eca02bb58419d3aef0873be0b74b86ab33dd0f5
renchao <renchao@admin.com>
2022-04-21 14:59:55 +0800

    迭代器模式-1.0

---
+++ src/com/renchao/iterator/College.java
@@ -0,0 +1,14 @@
+package com.renchao.iterator;
+
+import java.util.Iterator;
+
+public interface College {
+	
+	String getName();
+	
+	//增加系的方法
+	void addDepartment(String name, String desc);
+	
+	//返回一个迭代器,遍历
+	Iterator<Department>  createIterator();
+}
